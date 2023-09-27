c1da4c7992fa09c826e6ab5f7d01d7540a58c52a
iwanka <lutsiukivanna268@gmail.com>
2023-09-27 20:42:14 +0300

    first commit

---
+++ get_nouns.py
@@ -0,0 +1,16 @@
+import random
+
+from imports import nlp
+
+
+def get_nouns(article):
+  doc = nlp(article)
+  nouns = []
+  for token in doc:
+    if token.pos_ == "PROPN" or token.pos_ == "NOUN":
+      nouns.append(token.text)
+  return nouns
+
+def reflect_noun(noun):
+  mirror_phrases = ["... Interesting...", "? Hmm... ", "? Really?", "? Doesn't sound right...", ]
+  return noun.title() + random.choice(mirror_phrases)
---
+++ imports.py
@@ -0,0 +1,6 @@
+import spacy
+from textblob import TextBlob
+import random
+nlp = spacy.load("en_core_web_sm")
+greet = ["Hi!", "Whassup?", "Tsup!","What’s cooking?", "What’s wrong friend’?"]
+dummy_phrases = ["Noooo", "What?", "How come?" , "Thaat's insane", "No way", "Makes zero sense" , "Go on", "You're not serious, right?",  "Pepsi or Coke?"]
